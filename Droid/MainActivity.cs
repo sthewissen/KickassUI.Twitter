e8ca2a0ac62800f8c79a9d672c0c6338f6369316
sthewissen <github@thewissen.io>
2017-08-24 14:19:31 +0200

    Added app icons

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 
 namespace KickassUI.Twitter.Droid
 {
-    [Activity(Label = "KickassUI.Twitter.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "KickassUI Twitter", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
